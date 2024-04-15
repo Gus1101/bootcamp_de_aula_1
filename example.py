5992c5c14bbd88215469cc99f83360e278355caa
Gus1101 <augustosantosb60@gmail.com>
2024-04-15 13:55:49 -0300

    Inclusão da feature para soma de dois  valores sem tratamento de exceções

--- example.py
+++ example.py
@@ -1,3 +1,12 @@
+#Programa para retornar uma mensagem com o nome e o tamanho do mesmo
+
 nome = str(input("Bom dia, amigo! Qual o seu nome: ").capitalize().strip())
 print("Tenha uma ótima manhã,{}".format(nome))
 print(len(nome))
+
+#Programa para retornar a some de dois valores
+
+valor_1 = int(input("Digite o primeiro valor: "))
+valor_2 = int(input("Digite o segundo valor: "))
+sum_valor = valor_1 + valor_2
+print(sum_valor)
