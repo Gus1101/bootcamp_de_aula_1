dfc2ac7e68ec6fb7e7d5c2eaca8e19c4f4df95c0
Gus1101 <augustosantosb60@gmail.com>
2024-04-15 15:53:14 -0300

    Inserção da feature para calculo de bônus, sem tratamento de exceções

--- example.py
+++ example.py
@@ -4,9 +4,19 @@ nome = str(input("Bom dia, amigo! Qual o seu nome: ").capitalize().strip())
 print("Tenha uma ótima manhã,{}".format(nome))
 print(len(nome))
 
-#Programa para retornar a some de dois valores
+#Programa para retornar a soma de dois valores
 
 valor_1 = int(input("Digite o primeiro valor: "))
 valor_2 = int(input("Digite o segundo valor: "))
 sum_valor = valor_1 + valor_2
 print(sum_valor)
+
+#Programa para retornar calculo de kpi simples
+
+nome  = str(input("Digite seu nome: "))
+salario = float(input("Digite o seu salario: "))
+bonus = float(input("Digite o seu bônus: " ))
+
+vl_bonus = 1000 + salario *  1 + (bonus/100)
+
+print("Olá, {}. Seu bônus é de {}".format(nome,vl_bonus))
