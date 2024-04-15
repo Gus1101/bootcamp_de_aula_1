801410f3f5f47e357c7ab697284371080abaacc7
Gus1101 <augustosantosb60@gmail.com>
2024-04-15 15:56:32 -0300

    mudanças nas linhas 4 e 22 para melhor legibilidade do programa

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 #Programa para retornar uma mensagem com o nome e o tamanho do mesmo
 
 nome = str(input("Bom dia, amigo! Qual o seu nome: ").capitalize().strip())
-print("Tenha uma ótima manhã,{}".format(nome))
+print(f"Tenha uma ótima manhã,{nome}")
 print(len(nome))
 
 #Programa para retornar a soma de dois valores
@@ -19,4 +19,4 @@ bonus = float(input("Digite o seu bônus: " ))
 
 vl_bonus = 1000 + salario *  1 + (bonus/100)
 
-print("Olá, {}. Seu bônus é de {}".format(nome,vl_bonus))
+print(f"Olá, {nome}. Seu bônus é de {vl_bonus}")
